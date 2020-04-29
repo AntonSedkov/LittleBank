a58dd767893593a4d1358acdf376a539a21ef419
Antonius <S3dk0vAG@gmail.com>
2020-04-29 21:37:49 +0300

    add copied files
    add copied files

---
+++ javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"domain","c":"Account","l":"Account(double)"},{"p":"domain","c":"Account","l":"deposit(double)"},{"p":"domain","c":"Account","l":"getBalance()"},{"p":"domain","c":"Account","l":"withdraw(double)"}]
